3d391ff779bad15e541dd4c4898f01c9122c257c
billwk254 <billkihungu51@gmail.com>
2023-09-01 05:09:47 +0300

    Changes to 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
@@ -11,9 +10,12 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 size_t left_size, right_size;
+
 if (tree == NULL)
 return (0);
-size_t left_size = binary_tree_size(tree->left);
-size_t right_size = binary_tree_size(tree->right);
+left_size = binary_tree_size(tree->left);
+right_size = binary_tree_size(tree->right);
+
 return (left_size + right_size + 1);
 }
+
